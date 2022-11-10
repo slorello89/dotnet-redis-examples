4ba2152eb87140a71fca1736d0d600bd64aad32a
slorello89 <steve.lorello@redislabs.com>
2022-11-10 09:06:23 +0100

    adding console writes, cleaning up code

--- BasicRedisCommands/Program.cs
+++ BasicRedisCommands/Program.cs
@@ -4,36 +4,49 @@ using StackExchange.Redis;
 var muxer = ConnectionMultiplexer.Connect("localhost");
 var db = muxer.GetDatabase();
 
+//Wipe out the whole redis instance
+db.Execute("FLUSHDB");
+
 // sets a string in Redis
 db.StringSet("foo", "bar", TimeSpan.FromSeconds(50));
 
 // gets a string from Redis
 var stringGetResult = db.StringGet("foo");
+Console.WriteLine($"Current value of foo: {stringGetResult}");
 
 // adds an item to a set in Redis
 db.SetAdd("myset", "thing-1");
+db.SetAdd("myset", "thing-3");
 db.SetAdd("myset2", "thing-2");
 
 // checks if an item exists in a set
 var inSet = db.SetContains("myset", "thing-1");
+var str = inSet ? "was" : "was not";
+Console.WriteLine($"thing-1 {str} in myset");
 
 // gets the members of a set
 var setMembers = db.SetMembers("myset");
+Console.WriteLine($"My set members: {string.Join(',', setMembers)}");
 
 // unions two sets together
-db.SetCombine(SetOperation.Union,"myset","myset2");
+var union = db.SetCombine(SetOperation.Union,"myset","myset2");
+Console.WriteLine($"Union members: {string.Join(',', union)}");
 
 // adds steve with a time to the sorted set
-db.SortedSetAdd("users:last_visited", "steve", 1651259876 );
+db.SortedSetAdd("users:last_visited", "Steve", 1651259876 );
+db.SortedSetAdd("users:last_visited", "Bob", 1651259876 );
 
 // gets all the members of a sorted set
-db.SortedSetRangeByRank("users:last_visited");
+var usersLastVisited = db.SortedSetRangeByRankWithScores("users:last_visited");
+Console.WriteLine(string.Join(',', usersLastVisited));
+
 
 // sets a hash with fields within the hash
 db.HashSet("dog:1", new HashEntry[] {new ("name", "Honey"), new ("breed", "Greyhound")});
 
 // gets a field from a hash
-db.HashGet("dog:1", "name");
+var name = db.HashGet("dog:1", "name");
+Console.WriteLine($"dog:1 name from hash: {name}");
 
 db.KeyDelete("dog:1");
 
@@ -43,17 +56,18 @@ db.Execute("JSON.SET", "dog:1", "$", "{\"name\":\"Honey\",\"breed\":\"Greyhound\
 // gets a field from the JSON object in Redis
 db.Execute("JSON.GET", "dog:1", "$.breed");
 
+Console.WriteLine($"dog:1 name from JSON: {name}");
+
 // adds a message to a sensor's stream
 db.StreamAdd("sensor:1", "temp", 27);
 
 // reads a single item off of a stream
-db.StreamRead("sensor:1", 0);
+var msg = db.StreamRead("sensor:1", 0).First();
+Console.WriteLine($"Stream message {msg.Id}: {msg.Values.First().Name}, {msg.Values.First().Value}" );
 
-var info = db.StreamGroupInfo("sensor:1");
-if (!info.Any(g => g.Name == "avg"))
-{
-    db.StreamCreateConsumerGroup("sensor:1", "avg");
-}
+// Create the consumer group
+db.StreamCreateConsumerGroup("sensor:1", "avg", 0);
 
 // reads a stream in the context of a group
-db.StreamReadGroup("sensor:1", "avg", "con1", ">");
+msg = db.StreamReadGroup("sensor:1", "avg", "con1", ">").First();
+Console.WriteLine($"Stream message {msg.Id}: {msg.Values.First().Name}, {msg.Values.First().Value}" );
